c5c8df35b811294d77f7ef9ad5105e11da14638d
trushna116 <98582757+trushna116@users.noreply.github.com>
2022-01-28 15:46:21 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,14 @@
+#Add function implementation
+def add(x,y):
+    pass 
+#substract function implementation
+def substract(x,y):
+    pass 
+#multiply implementation
+def multiply(x,y):
+    pass
+# divide implementation
+def divide(x,y):
+    pass 
+
+    
